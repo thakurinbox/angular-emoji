981d19aa019e97c7979c17a0e587a162a67ae5a7
Naresh Thakur <thakurinbox@gmail.com>
2017-04-25 14:01:14 +0530

    emoji path

--- src/emoji.pipe.ts
+++ src/emoji.pipe.ts
@@ -20,17 +20,17 @@ export class EmojiPipe implements PipeTransform {
     if(filterBy == "unicode"){
         for (let key in this.emotions) {
             let re = new RegExp(key, 'ig');
-            value = value.replace(re, '<img src="node_modules/angular-emoji/src/emoji/png/' + key + '.png"' + 'class="emogi-image"' + ' title="' + this.emotions[key].name + '">');
+            value = value.replace(re, '<img src="node_modules/angular-emoji/emoji/png/' + key + '.png"' + 'class="emogi-image"' + ' title="' + this.emotions[key].name + '">');
         }
     }else if(filterBy == "image") {
         for (let key in this.emotions) {
             let re = new RegExp(key + ".png", 'ig');
-            value = value.replace(re, '<img src="node_modules/angular-emoji/src/emoji/png/' + key + '.png"' + 'class="emogi-image"' + ' title="' + this.emotions[key].name + '">');
+            value = value.replace(re, '<img src="node_modules/angular-emoji/emoji/png/' + key + '.png"' + 'class="emogi-image"' + ' title="' + this.emotions[key].name + '">');
         }
     }else{
       for (let key in this.emotions) {
         let re = new RegExp(this.emotions[key].shortname, 'ig');
-        value = value.replace(re, '<img src="node_modules/angular-emoji/src/emoji/png/' + key + '.png"' + 'class="emogi-image"' + ' title="' + this.emotions[key].name + '">');
+        value = value.replace(re, '<img src="node_modules/angular-emoji/emoji/png/' + key + '.png"' + 'class="emogi-image"' + ' title="' + this.emotions[key].name + '">');
       }
     }
     return value;
