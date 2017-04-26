b4e69224fcd428ac958ae21b91b2546631731210
Naresh Thakur <thakurinbox@gmail.com>
2017-04-26 18:46:16 +0530

    replace image with shortname

--- src/emoji.service.ts
+++ src/emoji.service.ts
@@ -15,7 +15,7 @@ export class EmojiService {
       }
     }else if(filterBy == "image") {
       for (let key in this.emotions) {
-        let re = new RegExp(key + ".png", 'ig');
+        let re = new RegExp('<img src="node_modules/angular-emoji/emoji/png/' + key + '.png">', 'ig');
         value = value.replace(re, this.emotions[key].shortname);
       }
     }else{
