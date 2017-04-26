c643711228b2c03d5eed56e5d22b9b82d3a2bc35
Naresh Thakur <thakurinbox@gmail.com>
2017-04-26 06:31:12 +0530

    text input changes

--- src/emoji.component.ts
+++ src/emoji.component.ts
@@ -54,6 +54,7 @@ export class EmojiComponent implements OnInit, OnChanges  {
   }
 
   onEmojiClick(e) {
+    console.log('onEmojiclick');
     this.input = this.input + ' <img src="node_modules/angular-emoji/emoji/png/' + e.code_points.base + '.png">';
     this.modelChange.emit(this.input);
     this.popupOpen = false;
