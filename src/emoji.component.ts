902e7f957211d61995cbc3ed9d2fbf4f91a72107
Naresh Thakur <thakurinbox@gmail.com>
2017-04-27 16:13:10 +0530

    reset text on enter

--- src/emoji.component.ts
+++ src/emoji.component.ts
@@ -27,6 +27,7 @@ export class EmojiComponent implements OnInit, OnChanges  {
     // remove extra lines
     let text = $event.target.outerText.replace(/(\r\n|\n|\r)/gm,"");
     console.log("text",text);
+    $event.target.outerText = '';
     this.input = '';
    // this.modelChange.emit(this.input);
     console.log('on enter');
